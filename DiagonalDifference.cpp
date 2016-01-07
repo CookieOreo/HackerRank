633ea14104d86b53173bb2b66fe487499feb8c1f
Zhen Ye Chen <zhenyechen@yahoo.com>
2016-01-06 23:40:58 -0500

    4th HackerRank Challenge

--- DiagonalDifference.cpp
+++ DiagonalDifference.cpp
@@ -76,12 +76,16 @@ int main(){
        for(int a_j = 0;a_j < n;a_j++){
           cin >> a[a_i][a_j];
           if(a_i == a_j){
+              //finds the number needed for primary diagonal and directly adds it. They're at (1,1),(2,2),(3,3),(4,4),...,(n,n).
               primary += a[a_i][a_j];
           }
        }
+       //finds the number needed for secondary diagonal and directly adds it. They're at (1,n),(2,n-1),(3,n-2),(4,n-3),...,(n,n-(n-1)).
        secondary += a[a_i][n-a_i-1];
     }
+    //calculates the difference of the primary and secondary diagonals
     difference = primary - secondary;
+    //checks if it's a negative, if so make it positive (i know i could use absolute value function but this is just easier to understand)
     if (difference < 0){
         difference *= -1;
     }
