43bd6e9c03cfd3696be6b3f78325104fae6f50b1
Zhen Ye Chen <zhenyechen@yahoo.com>
2016-01-07 23:34:49 -0500

    3rd HackerRank Challege

--- VeryBigSum.cpp
+++ VeryBigSum.cpp
@@ -54,11 +54,13 @@ using namespace std;
 
 int main(){
     int n;
+    //making it a long long so it can store large integer values so it doesn't lose its values
     long long sum = 0;
     cin >> n;
     vector<int> arr(n);
     for(int arr_i = 0;arr_i < n;arr_i++){
        cin >> arr[arr_i];
+       //adds it to this very large sum
        sum += arr[arr_i];
     }
     cout << sum;
